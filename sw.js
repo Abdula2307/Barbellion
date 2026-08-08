5831748a6b19f3562b38f91207fe5e07b1e7c817
Muhammad Abdullah <m.abdula.idrees@gmail.com>
2026-08-08 13:14:09 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Pass network requests through normally
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
