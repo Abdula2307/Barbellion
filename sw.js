b19c67226ec4c21fa379ea7536ef1dd1d14f4b6b
Muhammad Abdullah <m.abdula.idrees@gmail.com>
2026-08-10 07:37:43 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => self.clients.claim());
 self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return self.clients.openWindow('/dashboard.html');
+    })
+  );
+});
